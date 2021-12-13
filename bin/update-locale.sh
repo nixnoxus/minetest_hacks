127095b61175a29d257edd13195d1cb0a730ae03
nixnoxus <nixnoxus@web.de>
2021-12-13 22:34:38 +0100

    updates locale also from template.txt

--- bin/update-locale.sh
+++ bin/update-locale.sh
@@ -7,6 +7,7 @@
 
 OPT_DIFF=
 OPT_GIT=
+OPT_VERBOSE=
 
 error () { # <MESSAGE>
     echo "ERROR: $*" >&2
@@ -25,17 +26,14 @@ convert_tr () { # <FILE>
     done
     script=';s|^\(.\+\S\)\s*=\s*\(.*\)|\1=\2|g'"$cr$script"
     #script=';s/\([@=]\|\\n\)/@&/g'"$cr$script"
-    #script=';s/\xc2\xa0/ /g'
-    #script=';s/%%/%/g'
+    #script=';s/\xc2\xa0/ /g'"$cr$script"
+    #script=';s/%%/%/g'"$cr$script"
+    #script=';s/\r//g'"$cr$script"
 
     grep -q "^[^#]*=.*=" "$file" && error "bug: can't handle '=' in text:("
     grep -qF "$magic" "$file" || script="1 i $magic\n$cr$script"
-    if [ "$OPT_DIFF" = yes ]; then
-        sed "$script" "$file" | diff -u "$file" - || test $? -eq 1
-    else
-        echo "convert $file"
-        sed -i "$script" "$file"
-    fi
+    test "$OPT_VERBOSE" = yes && echo "convert $file"
+    sed "$script" "$file" > "$file.tmp"
 }
 
 update_locale () { # [DIR]
@@ -50,9 +48,8 @@ update_locale () { # [DIR]
     test -f "$template" || error "template '$template' not exists"
 
     files=("$template")
-
     local src dst
-    test "$OPT_DIFF" = yes || for dst in "$dir/locale/$modname".??{,_??}.tr
+    for dst in "$dir/locale/$modname".??{,_??}.tr
     do  test -f "$dst" && files[${#files[*]}]="$dst"
     done
 
@@ -64,44 +61,59 @@ update_locale () { # [DIR]
         if [ "$OPT_DIFF" = yes ]; then
             dst="$src"
         elif [ ! -f "$dst" ]; then
+            echo "moving $src to $dst"
             if [ "$OPT_GIT" = yes ]; then
                 git mv "$src" "$dst"
             else
                 mv "$src" "$dst"
             fi
-            files[${#files[*]}]="$dst"
-            #convert_tr "$dst" || exit $?
         fi
+        files[${#files[*]}]="$dst"
     done
 
-    #convert_tr "$template" || exit $?
     for dst in "${files[@]}"
     do  convert_tr "$dst" || exit $?
     done
 
-    xgettext -L Lua --from-code=UTF-8 --keyword=S -o - $(find "$dir" -name \*.lua) \
-        | sed 's@^msgid "\(.*\)"@\1@;tn;d;:n;s/\\"/"/g' \
-        | { rc=0
+    local rc
+    {
+        xgettext -L Lua --from-code=UTF-8 --keyword=S -o - $(find "$dir" -name \*.lua) \
+        | sed 's@^msgid "\(.*\)"@\1@;tn;d;:n;s/\\"/"/g' &&
+        test "${PIPESTATUS[*]}" = "0 0" && sed 's@^\([^#]*\)=@\1@;tn;d;:n' "${files[0]}.tmp"
+    } | {   rc=0
+            count=()
             while read -r line
-            do  for file in "${files[@]}"
-                do  grep -qF "$line=" "$file" && continue
-                    case "$line" in
-                    *%[dfs]*)
-                        echo "WARNING: illegal placeholder found. message ignored: $line" >&2
-                        rc=1
-                        continue
-                        ;;
-                    esac
-                    if [ "$OPT_DIFF" = yes ]; then
-                        echo "+$line="
-                    else
-                        echo "$line=" >> "$file"
-                    fi
+            do  case "$line" in
+                *%[dfs]*)
+                    echo "WARNING: illegal placeholder found. message ignored: $line" >&2
+                    rc=1
+                    continue
+                    ;;
+                esac
+                i=${#files[*]}
+                while let i--
+                do  grep -qF "$line=" "${files[i]}.tmp" && continue
+                    let count[i]++ || {
+                        test "$OPT_DIFF" = yes || echo "update ${files[i]}"
+                        echo >> "${files[i]}.tmp"
+                    }
+                    echo "$line=" >> "${files[i]}.tmp"
                 done
             done
             test "$rc" -eq 0
         }
-    test "${PIPESTATUS[*]}" = "0 0 0"
+    test "${PIPESTATUS[*]}" = "0 0" && rc=0 || rc=$?
+
+    for dst in "${files[@]}"
+    do  if [ "$OPT_DIFF" = yes ]; then
+            diff -u "$dst" "$dst.tmp"
+            rm -f "$dst.tmp" || exit $?
+        else
+            mv -f "$dst.tmp" "$dst" || exit $?
+        fi
+    done
+
+    return $rc
 }
 
 while [ $# -ge 1 ]
@@ -116,11 +128,15 @@ do  case "$1" in
         cat <<EOT
 usage: $0 [OPTION]..
 options:
- -h, --help
- -d, --diff
-     --git
+ -h, --help     show this help
+ -d, --diff     show diff only
+     --git      use 'git mv' instead 'mv'
+ -v, --verbose  verbose mode
 EOT
         exit
+        ;;
+    -v|--verbose)
+        OPT_VERBOSE=yes
         ;;
     -*) error "unsupported option '$1'"
         ;;
